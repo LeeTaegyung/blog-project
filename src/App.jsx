8bd16695d7f34726f79c086d0cadd47a72b4312e
tasi <taegyung.e@gmail.com>
2024-06-06 23:50:53 +0900

    blog project start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div id="wrap">
+            <header id="header">header</header>
+            <header id="header">footer</header>
+        </div>
+    );
+}
+
+export default App;
